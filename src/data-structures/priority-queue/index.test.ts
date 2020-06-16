8c166f0323274a9a6d1f9fdc5cfd4ac0d107cdee
Nejc <nmursi2@gmail.com>
2020-06-16 15:24:02 +0200

    feat: implemented priority-queue

--- src/data-structures/priority-queue/index.test.ts
+++ src/data-structures/priority-queue/index.test.ts
@@ -1,53 +1,105 @@
 import createQueue from './';
 
-describe('Queue', () => {
-  it('should create a queue', () => {
+describe('PriorityQueue', () => {
+  it('should create a priority queue', () => {
     const queue = createQueue();
     expect(queue).toBeDefined();
-    expect(queue.items.toArray()).toEqual([]);
+    expect(queue.items.items).toEqual([]);
     expect(queue.toString()).toEqual('');
   });
 
   it('should enqueue an item', () => {
     const queue = createQueue();
-    const initialValues = [1, false, 'a', 2];
+    const initialValues = [
+      {value: 'a', priority: 1},
+      {value: 'b', priority: 3},
+      {value: 'c', priority: 2},
+      {value: 'd', priority: 6},
+    ];
 
-    initialValues.forEach((val) => queue.enqueue(val));
+    expect(queue.items.isEmpty()).toBeTruthy();
 
-    expect(queue.items.toArray()).toEqual(initialValues);
-    queue.enqueue('b');
-    expect(queue.items.toArray()).toEqual([...initialValues, 'b']);
+    initialValues.forEach((item) => queue.enqueue(item.value, item.priority));
+
+    expect(queue.items.isEmpty()).toBeFalsy();
+    expect(queue.peek()).toEqual('a');
+    queue.enqueue('e');
+    expect(queue.peek()).toEqual('e');
   });
 
   it('should dequeue an item', () => {
     const queue = createQueue();
-    const initialValues = [1, false, {key: 'test'}];
+    const initialValues = [
+      {value: 'a', priority: 1},
+      {value: 'b', priority: 3},
+      {value: 'c', priority: 2},
+      {value: 'd', priority: 6},
+    ];
 
-    initialValues.forEach((val) => queue.enqueue(val));
+    initialValues.forEach((item) => queue.enqueue(item.value, item.priority));
 
-    expect(queue.dequeue()).toEqual(1);
-    expect(queue.items.toArray()).toEqual([false, {key: 'test'}]);
-    expect(queue.dequeue()).toEqual(false);
-    expect(queue.dequeue()).toEqual({key: 'test'});
+    expect(queue.dequeue()).toEqual('a');
+    expect(queue.dequeue()).toEqual('c');
+    expect(queue.dequeue()).toEqual('b');
+    expect(queue.dequeue()).toEqual('d');
     expect(queue.dequeue()).toBeUndefined();
   });
 
-  it('should peek the first item in the queue', () => {
+  it('should update the priority of an item', () => {
     const queue = createQueue();
-    const initialValues = ['x', 'y', 'z'];
+    const initialValues = [
+      {value: 'task 1', priority: 1},
+      {value: 'task 2', priority: 3},
+      {value: 'task 3', priority: 2},
+      {value: 'task 4', priority: 6},
+    ];
 
-    expect(queue.peek()).toBeUndefined();
-    initialValues.forEach((val) => queue.enqueue(val));
+    initialValues.forEach((item) => queue.enqueue(item.value, item.priority));
 
-    expect(queue.peek()).toEqual('x');
-    queue.dequeue();
-    expect(queue.peek()).toEqual('y');
+    expect(queue.peek()).toEqual('task 1');
+    queue.changePriority('task 2', 0);
+    expect(queue.peek()).toEqual('task 2');
+    queue.changePriority('task 4', 2);
+    expect(queue.peek()).toEqual('task 2');
   });
 
-  it('should check if the queue is empty', () => {
+  it('should peek the first item in the priority queue', () => {
+    const queue = createQueue();
+    const initialValues = [
+      {value: 'a', priority: 1},
+      {value: 'b', priority: 0},
+    ];
+
+    expect(queue.peek()).toBeUndefined();
+    initialValues.forEach((item) => queue.enqueue(item.value, item.priority));
+
+    expect(queue.peek()).toEqual('b');
+    queue.dequeue();
+    expect(queue.peek()).toEqual('a');
+  });
+
+  it('should check if the priority queue is empty', () => {
     const queue = createQueue();
 
     expect(queue.isEmpty()).toBe(true);
-    expect(queue.enqueue(0).isEmpty()).toBe(false);
+    expect(queue.enqueue('task 1').isEmpty()).toBe(false);
+  });
+
+  it('should covert the priority queue to string', () => {
+    const queue = createQueue();
+
+    const initialValues = [
+      {value: 'task 1', priority: 1},
+      {value: 'task 2', priority: 3},
+      {value: 'task 3', priority: 2},
+      {value: 'task 4', priority: 6},
+    ];
+
+    initialValues.forEach((item) => queue.enqueue(item.value, item.priority));
+
+    expect(queue.toString()).toEqual('task 1, task 2, task 3, task 4');
+    expect(queue.toString((item) => `#${item.value}`, ' -> ')).toEqual(
+      '#task 1 -> #task 2 -> #task 3 -> #task 4',
+    );
   });
 });
--- src/data-structures/priority-queue/index.ts
+++ src/data-structures/priority-queue/index.ts
@@ -1,33 +1,73 @@
-import createList, {LinkedList} from '../linked-list';
+import {lessOrEqualThan} from '../../helpers/comparator';
+import createHeap, {Heap, ToStringCallback} from '../heap';
 
+export interface Item<T> {
+  value: T;
+  priority: number;
+}
 export interface PriorityQueue<T> {
-  items: LinkedList<T>;
-  enqueue(this: PriorityQueue<T>, item: T): PriorityQueue<T>;
+  items: Heap<Item<T>>;
+  enqueue(
+    this: PriorityQueue<T>,
+    value: T,
+    priority?: number,
+  ): PriorityQueue<T>;
   dequeue(this: PriorityQueue<T>): T | undefined;
+  changePriority(this: PriorityQueue<T>, item: T, priority: number): void;
   peek(this: PriorityQueue<T>): T | undefined;
   isEmpty(this: PriorityQueue<T>): boolean;
-  toString(this: PriorityQueue<T>, separator?: string): string;
+  toString(
+    this: PriorityQueue<T>,
+    callback?: ToStringCallback<Item<T>>,
+    separator?: string,
+  ): string;
 }
+
+// custom compare function for the min heap
+const compareFunction = <T>(item1: Item<T>, item2: Item<T>): boolean =>
+  lessOrEqualThan(item1.priority, item2.priority);
+// custom toString function
+const convertToString = <T>(item: Item<T>) => String(item.value);
 
 const queue = <T>(): PriorityQueue<T> => {
   const queueList: PriorityQueue<T> = {
-    items: createList(),
+    items: createHeap<Item<T>>({compareFunction}), // linked list for the queue
 
-    enqueue: function enqueue(item) {
-      this.items.push(item);
+    // add item to the end of the queue
+    enqueue: function enqueue(value, priority = 0) {
+      this.items.add({value, priority});
       return this;
     },
+
+    // remove first item from the queue
     dequeue: function dequeue() {
-      return this.items.shift();
+      return this.items.shift()?.value;
     },
+
+    // change priority of an item
+    changePriority: function changePriority(value, priority) {
+      const updatedItem = {value, priority};
+      const indexes = this.items.find(
+        updatedItem,
+        (item, needle) => item.value === needle.value,
+      );
+      indexes.forEach((index) => this.items.update(index, updatedItem));
+      return this;
+    },
+
+    // get first item in queue
     peek: function peek() {
-      return this.items.head?.value;
+      return this.items.peek()?.value;
     },
+
+    // check if the queue is empty
     isEmpty: function isEmpty() {
       return this.items.isEmpty();
     },
-    toString: function toString(separator) {
-      return this.items.toString(undefined, separator);
+
+    // convert queue to string
+    toString: function toString(callback, separator = ', ') {
+      return this.items.toString(callback || convertToString, separator);
     },
   };
 
