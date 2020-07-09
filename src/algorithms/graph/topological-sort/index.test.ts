fbc43b43a85a1a5c7ea88c6b468beb9622945ede
Nejc <nmursi2@gmail.com>
2020-07-09 16:08:05 +0200

    feat: topological sort

---
+++ src/algorithms/graph/topological-sort/index.test.ts
@@ -0,0 +1,63 @@
+import createGraph from '../../../data-structures/graph';
+import topologicalSort from './';
+
+describe('TopologicalSort', () => {
+  it('should perform topologicalSort on graph', () => {
+    const graph = createGraph({directed: true});
+
+    expect(topologicalSort(graph)).toBeUndefined();
+
+    graph.addVertex(0);
+    graph.addVertex(1);
+    graph.addVertex(2);
+    graph.addVertex(3);
+    graph.addVertex(4);
+    graph.addVertex(5);
+
+    graph.addEdge(0, 1);
+    graph.addEdge(0, 4);
+    graph.addEdge(2, 3);
+    graph.addEdge(2, 1);
+
+    expect(topologicalSort(graph)?.map((v) => v.key)).toEqual([
+      0,
+      1,
+      4,
+      2,
+      3,
+      5,
+    ]);
+
+    graph.addEdge(0, 5);
+
+    expect(topologicalSort(graph)?.map((v) => v.key)).toEqual([
+      0,
+      1,
+      4,
+      5,
+      2,
+      3,
+    ]);
+
+    graph.addVertex(6);
+    graph.addVertex(7);
+    graph.addVertex(8);
+
+    graph.addEdge(4, 3);
+    graph.addEdge(5, 6);
+    graph.addEdge(5, 7);
+    graph.addEdge(6, 8);
+
+    expect(topologicalSort(graph)?.map((v) => v.key)).toEqual([
+      0,
+      1,
+      4,
+      3,
+      5,
+      6,
+      8,
+      7,
+      2,
+    ]);
+  });
+});
---
+++ src/algorithms/graph/topological-sort/index.ts
@@ -0,0 +1,35 @@
+import {Graph, Key, Vertex} from '../../../data-structures/graph';
+import depthFirstSearch, {Callback} from '../depth-first-search';
+
+export default function topologicalSort<T>(
+  graph: Graph<T>,
+): Vertex<T>[] | undefined {
+  // is empty
+  if (graph.isEmpty()) return undefined;
+
+  const unvisited = new Map<Key, boolean>();
+  graph.getAllVertices().forEach((vertex) => {
+    unvisited.set(vertex.key, true);
+  });
+
+  const result: Vertex<T>[] = [];
+  const visited = new Map<Key, boolean>();
+  const visitCallback: Callback<T> = (_, vertex) => {
+    // add to result
+    result.push(vertex);
+    // add vertex to visited
+    visited.set(vertex.key, true);
+    // remove vertex from unvisited
+    unvisited.delete(vertex.key);
+    return true;
+  };
+
+  // do dfs on all unvisited vertices
+  while (unvisited.size > 0) {
+    const key = unvisited.keys().next().value;
+    // dfs
+    depthFirstSearch(graph, key, {visitCallback, visited});
+  }
+
+  return result;
+}
